e13c2e3f1d89f1ba1ed9c21877d5ba96959fbe19
Blackburn0 <hamzatadebayo5@gmail.com>
2024-07-12 11:53:18 +0100

    deployment

--- todos/settings.py
+++ todos/settings.py
@@ -24,10 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vb3kftc7d+)r-@5ay2e(6cnh+(@dr#d!kr3j^6u0^d$yyq$fsg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = ['.vercel.app', 'localhost']
 
 # Application definition
 
