9badc9a09f461c109a125d284a9bc0fd8204f22b
Blackburn0 <hamzatadebayo5@gmail.com>
2024-07-13 18:46:03 +0100

    latest update

--- todos/settings.py
+++ todos/settings.py
@@ -85,9 +85,9 @@ WSGI_APPLICATION = 'todos.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'django-db',
-        'USER': 'Hamzat',
-        'PASSWORD': '*Hamzat2003*',
+        'NAME': os.getenv('POSTGRES_DATABASE'),
+        'USER': os.getenv('POSTGRES_USER'),
+        'PASSWORD': os.getenv('POSTGRES_PASSWORD'),
         # 'HOST': os.getenv('POSTGRES_HOST'),
         # 'PORT': os.getenv('POSTGRES_DB_PORT'),
     }
