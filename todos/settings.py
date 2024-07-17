f2053c3177e7062a8af6409b158d85f9dd7b6f36
Hamzat Adebayo Hamzat <hamzatadebayo5@gmail.com>
2024-07-17 12:42:11 +0100

    Update settings.py

--- todos/settings.py
+++ todos/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-vb3kftc7d+)r-@5ay2e(6cnh+(@dr#d!kr3j^6u0^d$yyq$fsg'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = 'False'
+DEBUG = False
 
 ALLOWED_HOSTS = ['.vercel.app', '.now.sh', '127.0.0.1']
 
